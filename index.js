22a76c9e2235898b193733089d6da3d501e53769
Haoyuan Xu <haoyuan.xu@ufl.edu>
2024-09-23 01:37:57 -0400

    Zip Function Waiting for Debug

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { product, device } from "./search.js";
-import { mkdir, writeFile } from "node:fs/promises";
+import { mkdir, readdir, writeFile } from "node:fs/promises";
+import { promisify } from "node:util";
+import { spawn } from "node:child_process";
 
 // Search example
 const res = await product("LM324");
@@ -13,7 +15,32 @@ const devices = Object.assign(
   ))
 );
 
-await mkdir("var", { recursive: true }).catch(() => {});
-await mkdir("var/FOOTPRINT", { recursive: true }).catch(() => {});
-await mkdir("var/SYMBOL", { recursive: true }).catch(() => {});
-await writeFile("var/devices.json", JSON.stringify(devices, null, 2));
+await mkdir("var", { recursive: true }).catch(() => { });
+await mkdir("var/FOOTPRINT", { recursive: true }).catch(() => { });
+await mkdir("var/SYMBOL", { recursive: true }).catch(() => { });
+await writeFile("var/devices.json", JSON.stringify({ devices }, null, 2));
+
+// dump the symbol and footprint data
+await Promise.all(
+  res
+    .map(async (r) => {
+      const sym = r.device_info?.symbol_info;
+      const ftp = r.device_info?.footprint_info;
+
+      if (sym?.dataStr && ftp?.dataStr) {
+        const symPath = `var/SYMBOL/${sym.uuid}.esym`;
+        await writeFile(symPath, sym.dataStr);
+        const fpPath = `var/FOOTPRINT/${ftp.uuid}.esym`;
+        await writeFile(fpPath, ftp.dataStr);
+      }
+    })
+);
+
+const getDirectories = async source =>
+  (await readdir(source, { withFileTypes: true }))
+    .map(dirent => dirent.name)
+
+const directories = await getDirectories("var");
+console.log(directories);
+
+spawn("zip", ["-r", "LCSC.elibz", ...directories], { cwd: "var"});
